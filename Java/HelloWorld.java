68e6e065a1374690b4efa1e20ccbc9a62c8f827c
tronghieu220403 <tronghieu220403@gmail.com>
2023-05-11 12:19:09 +0700

    .

--- Java/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package Java;
-
-class HelloWorld {
-    public static void main(String[] args) {
-        
-    }
-}
--- Java/OperatingSystemInfo.java
+++ Java/OperatingSystemInfo.java
@@ -1,7 +1,14 @@
-package Project1;
+package Java;
+import java.util.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import java.io.File;
-import java.lang.management.ManagementFactory;
 
 class OperatingSystemInfo {
     private String name;
@@ -44,12 +51,34 @@ class OperatingSystemInfo {
         System.out.println((double)(mySystemInfo.diskSize/(double)(1024*1024*1024)));
         long freeMemSize = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getFreeMemorySize();
         System.out.println("Free memory (GB): " + (double)((freeMemSize/(double)(1024*1024*1024))));
-    
+        
         System.out.println(System.getenv("PROCESSOR_IDENTIFIER"));
         System.out.println(System.getenv("PROCESSOR_ARCHITECTURE"));
         System.out.println(System.getenv("PROCESSOR_ARCHITEW6432"));
         System.out.println(System.getenv("NUMBER_OF_PROCESSORS"));
 
+        Map<String, String> env = System.getenv();
 
+        LinkedHashMap<String, String> collect =
+                env.entrySet().stream()
+                        .sorted(Map.Entry.comparingByKey())
+                        .collect(
+                                Collectors.toMap(
+                                        Map.Entry::getKey,
+                                        Map.Entry::getValue,
+                                        (oldValue, newValue) -> oldValue,
+                                        LinkedHashMap::new)
+                        );
+
+        collect.forEach((k, v) -> System.out.println(k + ":" + v));
+        Components components = JSensors.get.components();
+
+        List<Cpu> cpus = components.cpus;
+        if (cpus != null) {
+            for (final Cpu cpu : cpus) {
+                System.out.println("Found CPU: " + cpu.name);
+            }
+        }
+    
     }
 }
