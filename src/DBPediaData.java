76f6ddba4f62b657c6908bc9d206b65a742b8e7c
tronghieu220403 <tronghieu220403@gmail.com>
2023-06-16 00:49:15 +0700

    Update DBPediaData.java

--- src/DBPediaData.java
+++ src/DBPediaData.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.json.JSONArray;
@@ -193,9 +194,24 @@ public class DBPediaData extends EntityHandling {
         
     }
 
+    public static String convertCamelCase(String input) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (Character.isUpperCase(c) && i > 0) {
+                output.append(" ");
+            }
+            output.append(Character.toLowerCase(c));
+        }
+        return output.toString();
+    }
+
     protected void syncData() throws Exception
     {
         String[] bigCategories = {"địa điểm du lịch, di tích lịch sử", "lễ hội văn hóa", "nhân vật lịch sử", "sự kiện lịch sử", "triều đại lịch sử"};
+        String wikiJsonPath = "E:/Code/Java/OOP_Project/saveddata/Wikipedia/EntityJson";
+        String wikiPropPath = "E:/Code/Java/OOP_Project/saveddata/Wikipedia/EntityProperties";
+
         HashSet<String> qIDHashSet = new HashSet<>();
         for (String bigCategory: bigCategories)
         {
@@ -206,7 +222,7 @@ public class DBPediaData extends EntityHandling {
                 qIDHashSet.add(fileName.replaceAll(".json",""));
             }
         }
-        String dbFolder = superpath + "EntityJson/";
+        String dbEntityFolder = superpath + "EntityJson/";
 
         JSONObject wikiUrlMapped = new JSONObject();
         JSONObject rawWikiUrlMapped = getJSONFromFile("E:/Code/Java/OOP_Project/saveddata/Wikipedia/WikiAnalys/URLToEntities.json");
@@ -216,53 +232,128 @@ public class DBPediaData extends EntityHandling {
             wikiUrlMapped.put(urlDecode(url), rawWikiUrlMapped.getString(url));
         }
 
-        String wikiJsonPath = "E:/Code/Java/OOP_Project/saveddata/Wikipedia/EntityJson";
-        String wikiPropPath = "E:/Code/Java/OOP_Project/saveddata/Wikipedia/EntityProperties";
-        HashSet<String> files = listAllFiles(dbFolder);
-        HashMap<String, String> selected = new HashMap<String, String>(); 
-        HashMap<String, String> selectedP = new HashMap<>();
-        for (String fileName: files)
+        JSONObject selected = new JSONObject(); 
+        JSONObject selectedP = new JSONObject();
+
+        if (!fileExist(superpath + "wikiMapped.json") || !fileExist(superpath + "wikiMappedProp.json"))
         {
-            String filePath = dbFolder + fileName;
-            String key1 = "", key2 = "";
-            JSONObject json = getJSONFromFile(filePath);
-            Iterator<String> keys = ((JSONObject) json).keys();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                JSONObject value = json.getJSONObject(key);
-                if (value.has("http://xmlns.com/foaf/0.1/primaryTopic"))
-                {
-                    key1 = key;
-                }
-                if (value.has("http://xmlns.com/foaf/0.1/isPrimaryTopicOf"))
-                {
-                    key2 = value.getJSONArray("http://xmlns.com/foaf/0.1/isPrimaryTopicOf").getJSONObject(0).getString("value");
-                }
-            }
-            if (!key1.equals(key2))
+            HashSet<String> files = listAllFiles(dbEntityFolder);
+
+            for (String fileName: files)
             {
-                print("Something wrong", key1, key2);
-                break;
-            }
-            key1 = unicodeDecode(key1).replace("http:", "https:");
-            if (wikiUrlMapped.has(key1))
-            {
-                String qID = wikiUrlMapped.getString(key1);
-                if (qIDHashSet.contains(qID))
-                {
-                    selected.put(fileName, qID);
-                }
-                else{
-                    String label = WikiData.getViLabel(qID, wikiJsonPath, wikiPropPath);
-                    if (!label.isEmpty())
+                String filePath = dbEntityFolder + fileName;
+                String key1 = "", key2 = "";
+                JSONObject json = getJSONFromFile(filePath);
+                Iterator<String> keys = ((JSONObject) json).keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    JSONObject value = json.getJSONObject(key);
+                    if (value.has("http://xmlns.com/foaf/0.1/primaryTopic"))
                     {
-                        selectedP.put(fileName, label);
+                        key1 = key;
+                    }
+                    if (value.has("http://xmlns.com/foaf/0.1/isPrimaryTopicOf"))
+                    {
+                        key2 = value.getJSONArray("http://xmlns.com/foaf/0.1/isPrimaryTopicOf").getJSONObject(0).getString("value");
                     }
                 }
-            }   
+                if (!key1.equals(key2))
+                {
+                    print("Something wrong", key1, key2);
+                    break;
+                }
+                key1 = unicodeDecode(key1).replace("http:", "https:");
+                if (wikiUrlMapped.has(key1))
+                {
+                    String qID = wikiUrlMapped.getString(key1);
+                    if (qIDHashSet.contains(qID))
+                    {
+                        selected.put(fileName, qID);
+                    }
+                    else{
+                        String label = WikiData.getViLabel(qID, wikiJsonPath, wikiPropPath);
+                        if (!label.isEmpty())
+                        {
+                            selectedP.put(fileName, label);
+                        }
+                    }
+                }   
+            }
+            writeFile(superpath + "wikiMapped.json", selected.toString(), false);
+            writeFile(superpath + "wikiMappedProp.json", selectedP.toString(), false);
         }
-        writeFile(superpath + "wikiMapped.json", (new JSONObject(selected)).toString(), false);
-        writeFile(superpath + "wikiMappedProp.json", (new JSONObject(selectedP)).toString(), false);
+        else
+        {
+            selected = getJSONFromFile(superpath + "wikiMapped.json");
+            selectedP = getJSONFromFile(superpath + "wikiMappedProp.json");
+        }
         
+        HashSet<String> propertyHashSet = new HashSet<>();
+        if (!fileExist(superpath + "AllProperties.txt"))
+        {
+            Iterator<String> keys = selected.keys();
+            while(keys.hasNext())
+            {
+                JSONObject json = getJSONFromFile(dbEntityFolder + keys.next());
+                Iterator<String> firstFloorKeys = json.keys();
+                while(firstFloorKeys.hasNext())
+                {
+                    String firstFloorKey = firstFloorKeys.next();
+                    JSONObject firstFloorJson = json.getJSONObject(firstFloorKey);
+                    Iterator<String> secondFloorKeys = firstFloorJson.keys();
+                    while(secondFloorKeys.hasNext())
+                    {
+                        String propertyStr = secondFloorKeys.next();
+                        if (propertyStr.contains("wiki")||propertyStr.contains("Wiki")) continue;
+                        if (propertyStr.contains("http://dbpedia.org/property/"))
+                        {
+                            String p = propertyStr.replace("http://dbpedia.org/property/", "");
+                            propertyHashSet.add(convertCamelCase(p));
+                        }
+                        else if (propertyStr.contains("http://dbpedia.org/ontology/"))
+                        {
+                            String p = propertyStr.replace("http://dbpedia.org/ontology/", "");
+                            propertyHashSet.add(convertCamelCase(p));
+                        }
+                    }
+                }
+            }
+            for (String propName: propertyHashSet)
+            {
+                writeFile(superpath + "AllProperties.txt", propName + "\n", true);
+            }
+        }
+        else
+        {
+            List<String> lines = readFileAllLine(superpath + "AllProperties.txt");
+            for (String line: lines)
+            {
+                propertyHashSet.add(line);
+            }
+        }
+
+        HashSet<String> files = listAllFiles(wikiPropPath);
+        JSONObject mappedWikiProp = new JSONObject();
+        for (String fileName: files)
+        {
+            if (!fileName.contains("P")) continue;
+            String pID = fileName.replace(".json", "");
+            String propViLabel = WikiData.getViLabel(pID, wikiPropPath, wikiPropPath);
+            if (propViLabel.isEmpty()) continue;
+            JSONObject json = getJSONFromFile(wikiPropPath + "/" + fileName);
+            JSONObject entity = json.getJSONObject("entities").getJSONObject(pID);
+            String propEngLabel = entity.getJSONObject("labels").getJSONObject("en").getString("value");
+            mappedWikiProp.put(propEngLabel, propViLabel);
+            JSONObject aliases = entity.getJSONObject("aliases");
+            if (!aliases.has("en")) continue;
+            JSONArray propEngAliases = aliases.getJSONArray("en");
+            for (Object propEngAlias: propEngAliases)
+            {
+                JSONObject propAliasObj = (JSONObject)propEngAlias;
+                String alias = propAliasObj.getString("value");
+                mappedWikiProp.put(alias, propViLabel);
+            }
+        }
+        writeFile(superpath + "MappedWikiProp.json", mappedWikiProp.toString(), false);
     }
 }
